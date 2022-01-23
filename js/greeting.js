c6ac987c4b1fcbc765e52d2e7b253df581926747
Daeyeol Ryu <dy@yoobato.com>
2022-01-23 22:45:31 +0900

    Add greeting

---
+++ js/greeting.js
@@ -0,0 +1,28 @@
+const USERNAME_KEY = "username";
+const HIDDEN_CLASSNAME = "hidden";
+
+const showGreeting = (username) => {
+  const greetingH1 = document.querySelector("#greeting");
+  greetingH1.innerText = `Hello, ${username}!`;
+  greetingH1.classList.remove(HIDDEN_CLASSNAME);
+};
+
+const handleLogin = (event) => {
+  event.preventDefault();
+
+  const usernameInput = document.querySelector("#usernameInput");
+  const username = usernameInput.value;
+
+  localStorage.setItem(USERNAME_KEY, username);
+  showGreeting(username);
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+};
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+if (savedUsername) {
+  showGreeting(savedUsername);
+} else {
+  const loginForm = document.querySelector("#loginForm");
+  loginForm.addEventListener("submit", handleLogin);
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+}
